3f7a0c6f5ba18d2ecbc86893f29e0851dd241264
cwong2015 <sirhc8002@gmail.com>
2017-12-24 03:16:31 -0500

    finished second microservice: timestamp

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+'use strict';
+
 var express = require('express');
+var moment = require('moment');
 
 var app = express();
 
 app.get('/*', function(req, res){
     var url = req.url;
-    if(isNaN(url)){
-        console.log("hello world");
+    var parsedNum = url.split('/');
+    var date = parsedNum[1];
+
+    var jsonDates = {
+        unix: null,
+        natural: null
+    };
+
+    if(!isNaN(date)){ // unix
+        var humanDate = moment.unix(date).format('MMMM DD, YYYY');
+        jsonDates.unix = date;
+        jsonDates.natural = humanDate;
     }
-    else{
-        console.log("fail");
+    else{ // check if human readable date
+        var splits = date.split("%20").join('');
+        
+        if(moment(splits, "MMMMDDYYYY").isValid()){
+            jsonDates.unix = moment(splits, "MMMMDDYYYY").format("X");
+            jsonDates.natural = moment(splits, "MMMMDDYYYY").format("MMMM DD, YYYY");
+        }
     }
+    res.json(jsonDates);
 });
 
 app.listen(3000);
